773b05b687f738820ef6c6b4ea19b91cbfb93933
mjlaurent <mjlaurent@connect.ust.hk>
2023-04-13 22:13:58 +0800

    worked

--- .docker-compose/postgresql/initdb/0003-init_chirpstack_integration.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    create role chirpstack_integration with login password 'chirpstack_integration';
-    create database chirpstack_integration with owner chirpstack_integration;
-EOSQL
